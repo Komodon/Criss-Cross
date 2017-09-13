337c4ef38e9f1fd47945240faa7d63c8d14b62b7
matteo.zampiccoli <matteo.zampiccoli@marconirovereto.it>
2017-09-13 15:41:12 +0200

    First commit

---
+++ 2017.05.12 Tris/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace _2017._05._12_Tris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCampo = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCampo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCampo
+            // 
+            this.dgvCampo.AllowUserToAddRows = false;
+            this.dgvCampo.AllowUserToDeleteRows = false;
+            this.dgvCampo.AllowUserToResizeColumns = false;
+            this.dgvCampo.AllowUserToResizeRows = false;
+            this.dgvCampo.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvCampo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCampo.Location = new System.Drawing.Point(66, 59);
+            this.dgvCampo.Name = "dgvCampo";
+            this.dgvCampo.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dgvCampo.Size = new System.Drawing.Size(400, 400);
+            this.dgvCampo.TabIndex = 0;
+            this.dgvCampo.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCampo_CellClick);
+            this.dgvCampo.CellPainting += new System.Windows.Forms.DataGridViewCellPaintingEventHandler(this.dgvCampo_CellPainting);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(186, 497);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 69);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "RIAVVIA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 599);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvCampo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCampo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCampo;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 2017.05.12 Tris/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2017._05._12_Tris
+{
+    public partial class Form1 : Form
+    {
+        int par;
+        bool reset;
+        Image cross = Image.FromFile("cross.png");
+        Image circle = Image.FromFile("circle.png");
+
+
+        string simbolo;
+        string[,] m = new string[3, 3];
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            dgvCampo.ColumnHeadersVisible = false;
+            dgvCampo.RowHeadersVisible = false;
+            
+            dgvCampo.ReadOnly = true;
+            dgvCampo.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgvCampo.DefaultCellStyle.Font = new Font("Calibri", 20);
+
+            dgvCampo.RowCount = 3;
+            for (int i = 0; i < 3; i++) dgvCampo.Rows[i].Height = dgvCampo.Height / 3;
+            
+            dgvCampo.ColumnCount = 3;
+            for (int i = 0; i < 3; i++) dgvCampo.Columns[i].Width = dgvCampo.Width / 3;
+            
+            InitCampo(ref m);
+            CopiaMatGrid(m);
+
+            simbolo = "X";
+            reset = false;
+        }
+
+        /// <summary>
+        /// Inizializza il 'campo' con string vuote
+        /// </summary>
+        /// <param name="c"></param>
+        void InitCampo(ref string[,] m)
+        {
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    m[r, c] = "";
+                }
+            }
+        }
+
+        /// <summary>
+        /// Controlla se c'è un Vincitore
+        /// </summary>
+        /// <param name="c"></param>
+        /// <param name="simbolo"></param>
+        /// <returns></returns>
+        bool ControllaVittoria(string[,] c, string simbolo)
+        {
+            par = 0;
+
+            if (c[0, 0] == simbolo && c[0, 1] == simbolo && c[0, 2] == simbolo) return true;
+            if (c[1, 0] == simbolo && c[1, 1] == simbolo && c[1, 2] == simbolo) return true;
+            if (c[2, 0] == simbolo && c[2, 1] == simbolo && c[2, 2] == simbolo) return true;
+            if (c[0, 0] == simbolo && c[1, 0] == simbolo && c[2, 0] == simbolo) return true;
+            if (c[0, 1] == simbolo && c[1, 1] == simbolo && c[2, 1] == simbolo) return true; 
+            if (c[0, 2] == simbolo && c[1, 2] == simbolo && c[2, 2] == simbolo) return true;
+            if (c[0, 0] == simbolo && c[1, 1] == simbolo && c[2, 2] == simbolo) return true;
+            if (c[0, 2] == simbolo && c[1, 1] == simbolo && c[2, 0] == simbolo) return true;
+
+            for (int r = 0; r < 3; r++) { for (int co = 0; co < 3; co++) { if(m[r,co] != "") par++; } }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Copia il valore della Matrice all'interno della DataGridView
+        /// </summary>
+        /// <param name="m"></param>
+        void CopiaMatGrid(string[,] m)
+        {
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    dgvCampo.Rows[r].Cells[c].Value = m[r, c];
+                }
+            }
+        }
+        
+        private void dgvCampo_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (m[e.RowIndex, e.ColumnIndex] == "")
+            {
+                reset = true;
+                m[e.RowIndex, e.ColumnIndex] = simbolo;
+
+                if (ControllaVittoria(m, simbolo) && par < 9)
+                {
+                    MessageBox.Show("Giocatore " + simbolo + " ha Vinto!");
+                    Restart();
+                }
+                else if (par >= 9)
+                {
+                    MessageBox.Show("PARITA'!");
+                    Restart();
+                }
+
+                if (simbolo == "X" && reset) simbolo = "O"; else simbolo = "X";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Restart();
+        }
+
+        /// <summary>
+        /// Ricomincia da Capo
+        /// </summary>
+        void Restart()
+        {
+            Form1 test = new Form1();
+            test.Show();
+            this.Hide();
+        }
+
+        private void dgvCampo_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            Rectangle cell = e.CellBounds;
+            
+            if (m[e.RowIndex, e.ColumnIndex] == "X")
+                e.Graphics.DrawImage(cross, cell);
+
+            else
+            if (m[e.RowIndex, e.ColumnIndex] == "O")
+                e.Graphics.DrawImage(circle, cell);
+
+            else e.Graphics.FillRectangle(Brushes.White, cell);
+            
+
+            e.Graphics.DrawRectangle(Pens.Black, cell);
+            e.Handled = true;
+        }
+    }
+}
